932150f3242a5745b486e4c64b3d19e9d8861cf5
Christian Schirin <Christian.Schirin@haw-hamburg.de>
2014-10-07 11:08:07 +0200

    Eclipse Projekt erstellt
    ich weiß immer noch nicht, ob das gut ist, 
    .classpath und .project miteinzuchecken 
    sollte hier keine probleme bereiten.

---
+++ src/computergraphics/applications/CGFrame.java
@@ -0,0 +1,54 @@
+/**
+ * Prof. Philipp Jenke
+ * Hochschule für Angewandte Wissenschaften (HAW), Hamburg
+ * Lecture demo program.
+ */
+package computergraphics.applications;
+
+
+import computergraphics.framework.AbstractCGFrame;
+import computergraphics.math.Vector3;
+import computergraphics.scenegraph.ColorNode;
+import computergraphics.scenegraph.SingleTriangleNode;
+
+/**
+ * Application for the first exercise.
+ * 
+ * @author Philipp Jenke
+ * 
+ */
+public class CGFrame extends AbstractCGFrame {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 4257130065274995543L;
+
+	/**
+	 * Constructor.
+	 */
+	public CGFrame(int timerInverval) {
+		super(timerInverval);
+		ColorNode colorNode = new ColorNode(new Vector3(0.25, 0.25, 0.75));
+		SingleTriangleNode triangleNode = new SingleTriangleNode();
+		getRoot().addChild(colorNode);
+		colorNode.addChild(triangleNode);
+	}
+
+	/*
+	 * (nicht-Javadoc)
+	 * 
+	 * @see computergrafik.framework.ComputergrafikFrame#timerTick()
+	 */
+	@Override
+	protected void timerTick() {
+		// System.out.println("Tick");
+	}
+
+	/**
+	 * Program entry point.
+	 */
+	public static void main(String[] args) {
+		new CGFrame(1000);
+	}
+}
