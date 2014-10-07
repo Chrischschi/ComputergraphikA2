3ed1f6a4347f4b6fb5e894be1abe461a2a7d3917
Andreas Mauritz <andreas.mauritz@haw-hamburg.de>
2014-10-07 11:13:59 +0200

    Klasse TriangleMeshImpl angelegt

---
+++ src/computergraphics/datastructures/TriangleMeshImpl.java
@@ -0,0 +1,48 @@
+package computergraphics.datastructures;
+
+// TODO ganze Klasse implementieren
+public class TriangleMeshImpl implements ITriangleMesh {
+
+	@Override
+	public void addTriangle(Triangle t) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public int addVertex(Vertex v) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int getNumberOfTriangles() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int getNumberOfVertices() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public Triangle getTriangle(int index) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Vertex getVertex(int index) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void clear() {
+		// TODO Auto-generated method stub
+
+	}
+
+}
